9338a086b0a75cd386e6fab742bf3f7bccb84575
Andy Gnias <AGnias47@gmail.com>
2022-11-04 15:06:18 -0400

    Update docstrings

--- string_helpers.h
+++ string_helpers.h
@@ -20,19 +20,40 @@ int array_contains_string(char **args, char *s);
 int array_contains_char_as_substring(char **args, char s);
 
 /**
- * @brief 
+ * @brief Splits an array of strings by a specified string in the array
  * 
- * @param args 
- * @param left 
- * @param right 
- * @param s 
+ * @param args array of strings
+ * @param left stores strings to the left of the specified string
+ * @param right stores strings to the right of the specified string
+ * @param s string used to split
  */
 void split_by_string(char **args, char **left, char **right, char *s);
 
+/**
+ * @brief Joins an array of strings
+ * 
+ * @param args array of strings
+ * @param str_container used to store result
+ */
 void join_str_array(char **args, char *str_container);
 
+/**
+ * @brief Splits an array of strings by a delimiter, where the right side of the 
+ * delimiter is a singe string corresponding to a file name
+ * 
+ * @param args array of strings
+ * @param left stores content to the left of the delimiter
+ * @param fname single string to the right of the delimeter
+ * @param delimiter delimeter
+ */
 void split_by_delimiter(char **args, char **left, char *fname, char *delimiter);
 
+/**
+ * @brief Returns the length of an array
+ * 
+ * @param array array
+ * @return int 
+ */
 int array_length(char **array);
 
 #endif
